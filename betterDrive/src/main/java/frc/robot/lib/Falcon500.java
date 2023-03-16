4f3738722a064a7ac8ab7e4f13c65341b8faf7f9
IcedTAddict <116768378+IcedTAddict@users.noreply.github.com>
2023-03-16 16:55:16 -0700

    Add files via upload

---
+++ betterDrive/src/main/java/frc/robot/lib/Falcon500.java
@@ -0,0 +1,62 @@
+package frc.robot.lib;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+
+public class Falcon500 implements MotorController {
+    TalonFX motor;
+    TalonFX motor2;
+    
+    public Falcon500(int talonFxID, int talonFxID2){
+        motor = new TalonFX(talonFxID);
+        motor2 = new TalonFX(talonFxID2);
+    }
+
+    @Override
+    public void set(double speed) {
+            motor.set(ControlMode.PercentOutput, speed);
+            motor.setNeutralMode(NeutralMode.Coast);
+            motor2.set(ControlMode.PercentOutput, speed);
+            motor2.setNeutralMode(NeutralMode.Coast);
+    }
+    
+    @Override
+    public double get() {
+        return (motor.getMotorOutputPercent()+motor2.getSelectedSensorPosition())/2;
+    }
+
+    @Override
+    public void setInverted(boolean isInverted) {
+        motor.setInverted(isInverted);
+        motor2.setInverted(isInverted);
+    }
+
+    public void setInverted(boolean isInverted, boolean isInverted2) {
+        motor.setInverted(isInverted);
+        motor2.setInverted(isInverted2);
+    }
+
+    @Override
+    public boolean getInverted() {
+        return motor.getInverted();
+    }
+
+    @Override
+    public void disable() {
+        motor.set(ControlMode.PercentOutput, 0);
+        motor.setNeutralMode(NeutralMode.Coast);
+        motor2.set(ControlMode.PercentOutput, 0);
+        motor2.setNeutralMode(NeutralMode.Coast);
+    }
+
+    @Override
+    public void stopMotor() {
+        motor.set(ControlMode.PercentOutput, 0);
+        motor.setNeutralMode(NeutralMode.Brake);
+        motor2.set(ControlMode.PercentOutput, 0);
+        motor2.setNeutralMode(NeutralMode.Brake);
+    }
+}
---
+++ betterDrive/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -0,0 +1,109 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Pose2d;
+// import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+// import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+
+// import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+// import edu.wpi.first.wpilibj.SpeedControllerGroup;
+// import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import frc.robot.Constants.driveTrain;
+import frc.robot.Constants.DriveTrainConstants;
+import frc.robot.lib.Falcon500;
+
+// import com.kauailabs.navx.frc.AHRS;
+// import edu.wpi.first.wpilibj.SerialPort;
+
+public class DriveTrainSubsystem extends SubsystemBase {
+  /**
+   * Creates a new ExampleSubsystem.
+   */
+  // private final AHRS navX = new AHRS(SerialPort.Port.kUSB);
+
+  Falcon500 frontRightFalcon = new Falcon500(DriveTrainConstants.rightFrontFalconID,DriveTrainConstants.rightBackFalconID);
+  // Falcon500 backRightFalcon = new Falcon500(DriveTrainConstants.rightBackFalconID);
+  Falcon500 frontLeftFalcon = new Falcon500(DriveTrainConstants.leftFrontFalconID,DriveTrainConstants.leftBackFalconID);
+  // Falcon500 backLeftFalcon = new Falcon500(DriveTrainConstants.leftBackFalconID);
+
+  DifferentialDrive m_driveTrain = new DifferentialDrive(frontLeftFalcon, frontRightFalcon);
+  
+  private final Gyro m_gyro = new Gyro() {
+
+    @Override
+    public void close() throws Exception {
+      // TODO Auto-generated method stub
+      
+    }
+
+    @Override
+    public void calibrate() {
+      // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void reset() {
+      // TODO Auto-generated method stub
+      
+    }
+
+    @Override
+    public double getAngle() {
+      // TODO Auto-generated method stub
+      return 0;
+    }
+
+    @Override
+    public double getRate() {
+      // TODO Auto-generated method stub
+      return 0;
+    }
+    
+  };
+  static DriveTrainSubsystem INSTANCE = new DriveTrainSubsystem();
+
+  //private final DifferentialDriveOdometry m_odometry;
+  public DriveTrainSubsystem() {
+    // frontRightFalcon.setInverted(true);
+    // backLeftFalcon.setInverted(true);
+    //m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d());
+  }
+
+  public void cartesianDrive(double fowardSpeed, double rotation, double garbo) {
+    frontRightFalcon.setInverted(true, true);
+    // frontRightFalcon.setInverted(false, false);
+    // backLeftFalcon.setInverted(false);
+    frontLeftFalcon.setInverted(false, false);
+    // backRightFalcon.setInverted(true);\
+    m_driveTrain.arcadeDrive(fowardSpeed, rotation*(-1));
+  }
+
+  // public void polarDrive(double forwardPower, double currentRotation, double rotationRate) {
+  //   m_driveTrain.drivePolar(forwardPower, currentRotation, rotationRate);
+  // }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public static DriveTrainSubsystem getInstance() {
+    return INSTANCE;
+  }
+
+public void resetOdometry(Pose2d initialPose) {
+}
+// public Pose2d getPose(){
+//   return m_odometry.getPoseMeters();} 
+// }
+}
